481278f591f6ace90fccf85cf6b441cf1047f6a5
Dave Revell <dave.revell@gmail.com>
2011-05-02 16:29:41 -0700

    Commit old PaxosSocketMultiplexer before teardown and rebuild

--- src/java/org/megalon/PaxosSocketMultiplexer.java
+++ src/java/org/megalon/PaxosSocketMultiplexer.java
@@ -1,18 +1,24 @@
 package org.megalon;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,6 +28,7 @@ import org.megalon.avro.AvroAcceptResponse;
 import org.megalon.avro.AvroPrepare;
 import org.megalon.avro.AvroPrepareResponse;
 import org.megalon.avro.AvroWALEntry;
+import org.megalon.messages.MegalonMsg;
 
 /**
  * Normal Avro RPC would have us send a request over a socket, wait for a
@@ -41,58 +48,149 @@ public class PaxosSocketMultiplexer {
 	SocketChannel schan;
 	long reqSerial = 0;
 	Host host;
-	Map<Long, RespWaiter> prepWaiters = new HashMap<Long, RespWaiter>();
+	String replica;
+	@SuppressWarnings("rawtypes")
+	Map<Long, RespWaiter> waiters = new ConcurrentHashMap<Long, RespWaiter>();
 	Log logger = LogFactory.getLog(PaxosSocketMultiplexer.class);
 	Thread reader;
+	long lastReconnectAttemptTime = 0;
+	public static final long reconnectAttemptIntervalMs = 5000;
 	
-	public static final byte MSG_PREPARE = 1;
-	public static final byte MSG_PREPARE_RESP = 2;
-	public static final byte MSG_ACCEPT = 3;
-	public static final byte MSG_ACCEPT_RESP = 4;
-	
-	// TODO weak refs
+	// TODO weak refs to waiters, caller may have given up
 	// TODO non-blocking
+	// TODO shouldn't re-encode for each host
 	
 	final DatumWriter<AvroPrepare> prepareWriter = 
 		new SpecificDatumWriter<AvroPrepare>(AvroPrepare.class); 
 	final DatumWriter<AvroAccept> acceptWriter = 
 		new SpecificDatumWriter<AvroAccept>(AvroAccept.class); 
 	
-	public PaxosSocketMultiplexer(Host host) throws IOException {
+	public PaxosSocketMultiplexer(Host host, String replica) {
 		this.host = host;
-		schan = SocketChannel.open(new InetSocketAddress(host.nameOrAddr, 
-				host.port));
+		this.replica = replica;
+		reconnect();
 		reader = new Thread() {
 			public void run() {
-				socketReadLoop();
+				while(true) {
+					socketReadAndHandle();
+				}
 			}
 		};
+		reader.setDaemon(true);
 		reader.start();
 	}
 	
-	protected void socketReadLoop() {
-		try {
-			InputStream is = schan.socket().getInputStream();
-			while(true) {
-				byte[] msgType = new byte[1];
-				is.read(msgType, 0, 1);
-				switch(msgType[0]) {
-				case MSG_PREPARE_RESP:
-					// TODO avro response decode and send to waiter
-					break;
-				case MSG_ACCEPT_RESP:
-					// TODO avro response decode and send to waiter
-					break;
-				default:
-					logger.warn("RPC response was an unrecognized msg type");
-					throw new IOException();
-				}
-			}
-		} catch (IOException e) {
-			logger.warn("IOException in RPC socket read loop");
+	protected synchronized boolean reconnect() {
+		long nowTimeMs = System.currentTimeMillis();
+		if(lastReconnectAttemptTime + reconnectAttemptIntervalMs < nowTimeMs) {
+			lastReconnectAttemptTime = nowTimeMs; 
 			try {
-				schan.close();
-			} catch (IOException ex) {}
+				this.schan = SocketChannel.open(new InetSocketAddress(
+						host.nameOrAddr, host.port));
+				logger.debug("Multiplexer connected to " + host);
+				return true;
+			} catch (IOException e) {
+				logger.info("Multiplexer connection to " + host + 
+						" had exception", e);
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * This is the main function for a thread that sits on a socket connected
+	 * to a Replication Server. When the remote server sends a response to an
+	 * RPC call, this function deserializes the result and passes it to any
+	 * RespWaiter that might be waiting for the response.
+	 */
+	protected void socketReadAndHandle() {
+		boolean doReconnect = false;
+		if(schan == null || !schan.isConnected()) {
+			doReconnect = true;
+		} else {
+			try {
+				logger.debug("socketReadAndHandle: RPC response listener " +
+						"running for remote host " + host);
+//				InputStream is = schan.socket().getInputStream();
+				BinaryDecoder dec = null;
+				
+				DatumReader<AvroPrepareResponse> prepareRespReader = new 
+					SpecificDatumReader<AvroPrepareResponse>(AvroPrepareResponse.class);
+				DatumReader<AvroAcceptResponse> acceptRespReader = new 
+					SpecificDatumReader<AvroAcceptResponse>(AvroAcceptResponse.class);
+			
+				AvroPrepareResponse prepareResp = new AvroPrepareResponse();
+				AvroAcceptResponse acceptResp = new AvroAcceptResponse();
+				ByteBuffer bufLenBuf = ByteBuffer.allocate(4);
+				while(true) {
+					bufLenBuf.clear();
+					if(schan.read(bufLenBuf) == 0) {
+						continue;
+					}
+					int bufLen = Util.bytesToInt(bufLenBuf.array());
+					// TODO The following allocation prob. causes GC pressure
+					ByteBuffer msgBuf = ByteBuffer.allocate(bufLen);
+					insistRead(msgBuf, bufLen);
+					logger.debug("Incoming bufLen: " + bufLen);
+					
+					ByteArrayInputStream bis = 
+						new ByteArrayInputStream(msgBuf.array());
+					dec = DecoderFactory.get().binaryDecoder(bis, dec);
+					
+					long reqSerial;
+					Object responseObj = null;
+					int msgType = bis.read();
+					logger.debug("Got RPC response msg " + msgType);
+					switch(msgType) {
+					case MegalonMsg.MSG_PREPARE_RESP:
+						prepareResp = prepareRespReader.read(prepareResp, dec);
+						reqSerial = prepareResp.reqSerial;
+						if(prepareResp.walEntry != null) {
+							responseObj = new WALEntry(prepareResp.walEntry);
+						}
+						break;
+					case MegalonMsg.MSG_ACCEPT_RESP:
+						acceptResp = acceptRespReader.read(acceptResp, dec);
+						reqSerial = acceptResp.reqSerial;
+						responseObj = acceptResp.acked;
+						break;
+					default:
+						logger.warn("RPC response was an unrecognized msg type");
+						throw new IOException();
+					}
+	
+					RespWaiter waiter = waiters.get(reqSerial);
+					// waiter may be a soft reference to a GC'ed object
+					if(waiter != null) {
+						waiter.response(replica, responseObj);
+					}
+				}
+			} catch (Exception e) {
+				doReconnect = true;
+				try {
+					schan.close();
+				} catch (IOException ex) {}
+				logger.warn("Exception in RPC socket read, reconnecting", e);
+			}
+		}
+		
+		if(doReconnect) {
+			if(!reconnect()) {
+				try {
+					Thread.sleep(100); // TODO should backoff
+				} catch (InterruptedException ex) {}
+			}
+		}
+	}
+	
+	/**
+	 * Read len bytes into the buffers, throwing an IOException if the read
+	 * returns prematurely.
+	 */
+	void insistRead(ByteBuffer buf, int len) throws IOException {
+		// TODO does this work? is it common to get fewer bytes than requested?
+		if(schan.read(buf) != len) {
+			throw new IOException("Unexpected short read");
 		}
 	}
 	
@@ -105,72 +203,90 @@ public class PaxosSocketMultiplexer {
 	 * If there has been a socket error, this call could fail. If this happens,
 	 * the caller should create another object and try again. (TODO correct?)
 	 */
-	void prepare(long walIndex, long n, 
-			RespWaiter<AvroPrepareResponse> waiter) {
-		AvroPrepare msg = new AvroPrepare();
-		msg.reqSerial = getNextSerial();
-		msg.n = n;
-		msg.walIndex = walIndex;
+	public void prepare(long walIndex, long n, RespWaiter<WALEntry> waiter)
+	throws IOException {
+//		AvroPrepare msg = new AvroPrepare();
+////		msg.reqSerial = getNextSerial();
+//		msg.n = n;
+//		msg.walIndex = walIndex;
+
+
 		// TODO pool these for less GC?
 		ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		// TODO cache encoders? they're not thread safe though
 		BinaryEncoder e = EncoderFactory.get().binaryEncoder(bao, null);
-		try {
-			bao.write(MSG_PREPARE);
-			prepareWriter.write(msg, e);
-			e.flush();
-			byte[] outgoingBytes = bao.toByteArray();
-			prepWaiters.put(msg.reqSerial, waiter);
-			schan.write(ByteBuffer.wrap(outgoingBytes));
-		} catch (IOException ex) {
-			throw new AssertionError(); // Can never happen
-		}
+		bao.write(MegalonMsg.MSG_PREPARE);
+		prepareWriter.write(msg, e);
+		e.flush();
+
+		waiters.put(msg.reqSerial, waiter); // Waiter will run if/when response
+		
+		writeLengthPrefixed(bao.toByteArray());
+		logger.debug("wrote prepare msg to replica: " + replica + " host: " + 
+				host);		
 	}
 	
 	/**
 	 * If there has been a socket error, this call could fail. If this happens,
 	 * the caller should create another object and try again. (TODO correct?)
 	 */
-	void accept(long walIndex, AvroWALEntry entry, 
-			RespWaiter<AvroAcceptResponse> waiter) {
+	public void accept(long walIndex, AvroWALEntry entry, 
+			RespWaiter<Object> waiter) throws IOException {
 		AvroAccept msg = new AvroAccept();
 		msg.walEntry = entry;
 		msg.reqSerial = getNextSerial();
 		msg.walIndex = walIndex;
 		ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		// TODO cache encoders? they're not thread safe though
 		BinaryEncoder e = EncoderFactory.get().binaryEncoder(bao, null);
-		try {
-			bao.write(MSG_ACCEPT);
-			acceptWriter.write(msg, e);
-			e.flush();
-			byte[] outgoingBytes = bao.toByteArray();
-			prepWaiters.put(msg.reqSerial, waiter);
-			schan.write(ByteBuffer.wrap(outgoingBytes));
-		} catch (IOException ex) {
-			waiter.nack(host);
+		bao.write(MegalonMsg.MSG_ACCEPT);
+		acceptWriter.write(msg, e);
+		e.flush();
+		byte[] outgoingBytes = bao.toByteArray();
+		writeLengthPrefixed(outgoingBytes);
+		logger.debug("Wrote accept msg to replica: " + replica + " host: " + 
+				host);
+	}
+	
+	/**
+	 * Given a byte array, write to the socket: 4 bytes of array length, then
+	 * the full array.
+	 */
+	void writeLengthPrefixed(byte[] bytes) throws IOException {
+		byte[] lengthBuf = Util.intToBytes(bytes.length);
+		ByteBuffer[] bufsToWrite = new ByteBuffer[] {
+				ByteBuffer.wrap(lengthBuf),
+				ByteBuffer.wrap(bytes)};
+		long nBytesWritten = schan.write(bufsToWrite);
+
+		logger.debug("RPC socket request sent nbytes: " + nBytesWritten);
+		logger.debug("Output length field: " + Arrays.toString(lengthBuf));
+		logger.debug("Output data field: " + Arrays.toString(bytes));
+		if(nBytesWritten != bytes.length + 4) {
+			throw new AssertionError();
 		}
-		
 	}
 	
 	static class RespWaiter<T> {
-		public Map<Host,T> responses;
+		public Map<String,T> responses;
 		int validResponses = 0;
 		int nackResponses = 0;
 		int numExpected;
 		Log logger = LogFactory.getLog(RespWaiter.class);
 		
 		public RespWaiter(int numExpected) {
-			responses = new HashMap<Host,T>();
+			responses = new HashMap<String,T>();
 			this.numExpected = numExpected;
 		}
 		
-		synchronized public void response(Host host, T response) {
-			responses.put(host, response);
+		synchronized public void response(String replica, T response) {
+			responses.put(replica, response);
 			validResponses++;
 			notify();
 		}
 		
-		synchronized public void nack(Host host) {
-			responses.put(host, null);
+		synchronized public void nack(String replica) {
+			responses.put(replica, null);
 			nackResponses++;
 			notify();
 		}
